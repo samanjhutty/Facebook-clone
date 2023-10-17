f3d2a1c09e76810f33c3fa129566ebc69e8527ed
Saman Jhutty <jhuttysaman@gmail.com>
2023-10-17 16:33:02 +0530

    minor changes

--- lib/model/messages_model.dart
+++ lib/model/messages_model.dart
@@ -1,11 +1,10 @@
-// ignore_for_file: avoid_print
-
 import 'dart:math';
 import 'package:facebook/assets/assets.dart';
 import 'package:facebook/assets/image_path.dart';
 import 'package:facebook/assets/strings.dart';
 import 'package:facebook/model/model.dart';
 import 'package:facebook/pages/profile.dart';
+import 'package:flutter/material.dart';
 
 List<AppModel> messagesData = [
   AppModel(
@@ -13,76 +12,76 @@ List<AppModel> messagesData = [
       username: Strings.friendsName[0],
       profileOnTap: () => gotoPage(page: const Profile()),
       data: Strings.newMessages[Random().nextInt(11)],
-      onTap: () => print('datas clicked'),
+      onTap: () => debugPrint('datas clicked'),
       text: '1d'),
   AppModel(
       profileImage: ImagePath.profileImage[Random().nextInt(11)],
       username: Strings.friendsName[1],
       profileOnTap: () => gotoPage(page: const Profile()),
       data: Strings.newMessages[Random().nextInt(11)],
-      onTap: () => print('datas clicked'),
+      onTap: () => debugPrint('datas clicked'),
       text: '1d'),
   AppModel(
       profileImage: ImagePath.profileImage[Random().nextInt(11)],
       username: Strings.friendsName[2],
       profileOnTap: () => gotoPage(page: const Profile()),
       data: Strings.newMessages[Random().nextInt(11)],
-      onTap: () => print('datas clicked'),
+      onTap: () => debugPrint('datas clicked'),
       text: '1d'),
   AppModel(
       profileImage: ImagePath.profileImage[Random().nextInt(11)],
       username: Strings.friendsName[3],
       profileOnTap: () => gotoPage(page: const Profile()),
       data: Strings.newMessages[Random().nextInt(11)],
-      onTap: () => print('datas clicked'),
+      onTap: () => debugPrint('datas clicked'),
       text: '1d'),
   AppModel(
       profileImage: ImagePath.profileImage[Random().nextInt(11)],
       username: Strings.friendsName[4],
       profileOnTap: () => gotoPage(page: const Profile()),
       data: Strings.newMessages[Random().nextInt(11)],
-      onTap: () => print('datas clicked'),
+      onTap: () => debugPrint('datas clicked'),
       text: '1d'),
   AppModel(
       profileImage: ImagePath.profileImage[Random().nextInt(11)],
       username: Strings.friendsName[5],
       profileOnTap: () => gotoPage(page: const Profile()),
       data: Strings.newMessages[Random().nextInt(11)],
-      onTap: () => print('datas clicked'),
+      onTap: () => debugPrint('datas clicked'),
       text: '1d'),
   AppModel(
       profileImage: ImagePath.profileImage[Random().nextInt(11)],
       username: Strings.friendsName[6],
       profileOnTap: () => gotoPage(page: const Profile()),
       data: Strings.newMessages[Random().nextInt(11)],
-      onTap: () => print('datas clicked'),
+      onTap: () => debugPrint('datas clicked'),
       text: '1d'),
   AppModel(
       profileImage: ImagePath.profileImage[Random().nextInt(11)],
       username: Strings.friendsName[7],
       profileOnTap: () => gotoPage(page: const Profile()),
       data: Strings.newMessages[Random().nextInt(11)],
-      onTap: () => print('datas clicked'),
+      onTap: () => debugPrint('datas clicked'),
       text: '1d'),
   AppModel(
       profileImage: ImagePath.profileImage[Random().nextInt(11)],
       username: Strings.friendsName[8],
       profileOnTap: () => gotoPage(page: const Profile()),
       data: Strings.newMessages[Random().nextInt(11)],
-      onTap: () => print('datas clicked'),
+      onTap: () => debugPrint('datas clicked'),
       text: '1d'),
   AppModel(
       profileImage: ImagePath.profileImage[Random().nextInt(11)],
       username: Strings.friendsName[9],
       profileOnTap: () => gotoPage(page: const Profile()),
       data: Strings.newMessages[Random().nextInt(11)],
-      onTap: () => print('datas clicked'),
+      onTap: () => debugPrint('datas clicked'),
       text: '1d'),
   AppModel(
       profileImage: ImagePath.profileImage[Random().nextInt(11)],
       username: Strings.friendsName[10],
       profileOnTap: () => gotoPage(page: const Profile()),
       data: Strings.newMessages[Random().nextInt(11)],
-      onTap: () => print('datas clicked'),
+      onTap: () => debugPrint('datas clicked'),
       text: '1d'),
 ];
